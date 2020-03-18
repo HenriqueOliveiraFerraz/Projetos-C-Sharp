c0ad00d4baa178cdaddff386647c8a785ce34324
HenryFe93 <henri.fna@gmail.com>
2020-03-18 20:28:59 -0300

    First Commit

---
+++ VetorClasse/VetorClasse/Estudantes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetorClasse
+{
+    class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Estudantes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
